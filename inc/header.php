2c07086b857e8d1e0305e8e3dcf9b1fc4d361e4d
tangeraymond <sonia.traore@ymail.com>
2022-07-18 08:31:40 +0200

    angetraore-dev

---
+++ inc/header.php
@@ -0,0 +1,54 @@
+<!-- As a heading
+<nav class="navbar navbar">
+    <span class="navbar-brand mb-0 h3">+225 0584 662 022</span>
+</nav>
+-->
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #CC33FF">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                    Dropdown link
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!--
+<div class="wrapheader">
+    <nav class="brandmenu">
+        <a class="navbar-brand" href="#">
+            <img src="/assets/logo/logo1a.JPG" width="30" height="30" class="d-inline-block align-top" alt="">
+
+        </a>
+
+        <a class="menu-link" href="">Accueil</a>
+        <a class="menu-link" href="">Services</a>
+        <a class="menu-link" href="">Projets</a>
+        <a class="menu-link" href="">Blog</a>
+        <a class="menu-link" href="">Contact</a>
+    </nav>
+
+    <div class="row">
+        <img style="height: 80px; width: 90px" alt="logo" src="../assets/logo/logo1b.JPG" />
+    </div>
+</div>
+-->
